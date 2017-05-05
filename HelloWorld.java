bfb39bc849bdbaedd65977026a8a0ea4d6c0a0e4
Daniel Widjaja <Daniel Widjaja>
2017-05-05 04:06:20 -0400

    add2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-	// Adding System.out.println
+	    // Adding System.out.println
         System.out.println("Praise the Lord");
+        
+		// Adding Prototype 2
+	    System.out.println("Content");
+		// Adding Prototype
     }
 
 }
